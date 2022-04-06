e1ec081a947324cc79ff7f3bbe0f1c5401821a1e
Paul Kirby <paulrdkirby@gmail.com>
2022-04-05 20:49:56 -0500

    setting up environment stuff

---
+++ esp8266_sketches/wifi_sketch/config.example.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG
+#define CONFIG
+
+#define WIFI_NAME "wifi_name_here"
+#define WIFI_PASS "wifi_password_here"
+
+#endif
--- esp8266_sketches/wifi_sketch/wifi_sketch.ino
+++ esp8266_sketches/wifi_sketch/wifi_sketch.ino
@@ -1,8 +1,32 @@
 #include "config.h"
 #include <ESP8266WiFi.h>
 
+// Following along with this guide: https://tttapa.github.io/ESP8266/Chap07%20-%20Wi-Fi%20Connections.html
+
+const char *wifiName = WIFI_NAME;
+const char *wifiPass = WIFI_PASS;
+
 void setup()
 {
+  Serial.begin(115200); // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+
+  WiFi.begin(wifiName, wifiPass);
+  Serial.print("Connecting to ");
+  Serial.print(wifiName);
+  Serial.println("....");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
